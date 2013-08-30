30336833d2629039c3ab37017a6ede1da2e02aa6
Mark <me@jsman.ru>
2013-08-30 09:30:20 +0300

    Factory

--- dominate.js
+++ dominate.js
@@ -3,7 +3,9 @@ module.exports = {
     Widget: require('./lib/Widget'),
     fromJSON : require('./lib/fromJSON'),
     utils: require('./lib/utils'),
+    factory: require('./lib/defaultFactory'),
     node: {
+        Text : require('./lib/Node/Text'),
         Img : require('./lib/Node/Img'),
         Default : require('./lib/Node/Default'),
     }
